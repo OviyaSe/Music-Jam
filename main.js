17316da4594625b40e1297d83cc4b29d3162ccb9
OviyaSe <77298020+OviyaSe@users.noreply.github.com>
2021-07-15 13:39:55 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function cow(){
+    document.getElementById("audio1").play();
+}
+function dog(){
+    document.getElementById("audio2").play();
+}
+function horse(){
+    document.getElementById("audio3").play();
+}
+function rooster(){
+    document.getElementById("audio4").play();
+}
+function sparrow(){
+    document.getElementById("audio5").play();
+}
